9d78e5dd67826fc32a205980a1e02649ab2a8389
Benoit Mrejen <benoit.mrejen@gmail.com>
2019-03-07 15:58:58 +0800

    feat: 17316 methods now in component

--- src/app/components/api-quickstart/api-quickstart.component.ts
+++ src/app/components/api-quickstart/api-quickstart.component.ts
@@ -8,31 +8,105 @@ import { ControlContainer, NgForm } from "@angular/forms";
   viewProviders: [{ provide: ControlContainer, useExisting: NgForm }],
 })
 
+// declare var gapi: any  ;
+
 export class ApiQuickStartComponent implements OnInit {
   public credentials;
+  // Client ID and API key from the Developer Console
+  public CLIENT_ID = "194265459541-u110u0hgudfv4k5irkoc6c83m5ph93q1.apps.googleusercontent.com";
+  public API_KEY = "AIzaSyBfeKvEwqrrFTpWA79_cLEGgJZYnRF1Dug";
+
+  // Array of API discovery doc URLs for APIs used by the quickstart
+  public DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/admin/directory_v1/rest"];
+
+  // Authorization scopes required by the API; multiple scopes can be
+  // included, separated by spaces.
+  public SCOPES = "https://www.googleapis.com/auth/admin.directory.user.readonly";
+
+  public authorizeButton = document.getElementById("authorize_button");
+  public signoutButton = document.getElementById("signout_button");
 
   constructor(private http: HttpClient) {
     //
   }
 
   public ngOnInit() {
-    const Modal = function() {
+    gapi.load("client:auth2", this.initClient);
+  }
 
-      // Client ID and API key from the Developer Console
-      const CLIENT_ID = "194265459541-u110u0hgudfv4k5irkoc6c83m5ph93q1.apps.googleusercontent.com";
-      const API_KEY = "AIzaSyBfeKvEwqrrFTpWA79_cLEGgJZYnRF1Dug";
+  // Initializes the API client library and sets up sign-in state listeners.
+  public initClient() {
+    gapi.client.init({
+      apiKey: this.API_KEY,
+      clientId: this.CLIENT_ID,
+      discoveryDocs: this.DISCOVERY_DOCS,
+      scope: this.SCOPES,
+    })
+    .then(function() {
+      // Listen for sign-in state changes.
+      gapi.auth2.getAuthInstance().isSignedIn
+      .listen(this.updateSigninStatus);
 
-      // Array of API discovery doc URLs for APIs used by the quickstart
-      const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/admin/directory_v1/rest"];
+      // Handle the initial sign-in state.
+      this.updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn
+                              .get());
+      this.authorizeButton.onclick = this.handleAuthClick;
+      this.signoutButton.onclick = this.handleSignoutClick;
+    }, function(error) {
+      this.appendPre(JSON.stringify(error, null, 2));
+    });
+  }
 
-      // Authorization scopes required by the API; multiple scopes can be
-      // included, separated by spaces.
-      const SCOPES = "https://www.googleapis.com/auth/admin.directory.user.readonly";
-      const obj = { CLIENT_ID, API_KEY, DISCOVERY_DOCS, SCOPES };
+  // Called when the signed in status changes, to update the UI appropriately. After a sign-in, the API is called.
+  public updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+      this.authorizeButton.style.display = "none";
+      this.signoutButton.style.display = "block";
+      this.listUsers();
+    } else {
+      this.authorizeButton.style.display = "block";
+      this.signoutButton.style.display = "none";
+    }
+  }
 
-    };
+  // Sign in the user upon button click.
+  public handleAuthClick(event) {
+    gapi.auth2.getAuthInstance()
+    .signIn();
+  }
 
-    const myModal = new Modal();
-    console.log("myModal", myModal);
+  // Sign out the user upon button click.
+  public handleSignoutClick(event) {
+    gapi.auth2.getAuthInstance()
+    .signOut();
+  }
+
+  // Display the results of the API call.
+  public appendPre(message) {
+    const pre = document.getElementById("content");
+    const textContent = document.createTextNode(message + "\n");
+    pre.appendChild(textContent);
+  }
+
+  // Print the first 10 users in the domain.
+  public listUsers() {
+    gapi.client.directory.users.list({
+      customer: "my_customer",
+      maxResults: 500,
+      orderBy: "email",
+    })
+    .then(function(response) {
+      const users = response.result.users;
+      this.appendPre("Users:");
+
+      if (users && users.length > 0) {
+        for (i = 0; i < users.length; i++) {
+          const user = users[i];
+          this.appendPre(`- ${user.primaryEmail} (${user.name.fullName})`);
+        }
+      } else {
+        this.appendPre("No users found.");
+      }
+    });
   }
 }
