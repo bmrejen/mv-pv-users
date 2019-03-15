661a392de8603c46594141c8c70d5743535c85e2
Benoit Mrejen <benoit.mrejen@gmail.com>
2019-03-15 12:44:13 +0800

    fix: removed viewprovider from component

--- src/app/components/gapi-users/gapi-users.component.ts
+++ src/app/components/gapi-users/gapi-users.component.ts
@@ -1,5 +1,4 @@
 import { AfterViewInit, Component } from "@angular/core";
-import { ControlContainer, NgForm } from "@angular/forms";
 
 declare var gapi: any;
 declare var initClient: any;
@@ -7,7 +6,6 @@ declare var initClient: any;
 @Component({
   selector: "mv-gapi-users",
   templateUrl: "./gapi-users.component.html",
-  viewProviders: [{ provide: ControlContainer, useExisting: NgForm }],
 })
 
 export class GapiUsersComponent implements AfterViewInit {
