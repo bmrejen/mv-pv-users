d351174014f546becd01304fa0d0cc2dc86b3557
Benoit Mrejen <benoit.mrejen@gmail.com>
2019-05-09 12:28:21 +0800

    fix: team bind ngmodel

--- src/app/components/teams/teams.component.ts
+++ src/app/components/teams/teams.component.ts
@@ -17,17 +17,7 @@ export class TeamsComponent {
     @Input() public teams;
     @Input() public sugarCurrentUser;
 
-    public handleClick(team): void {
-        if (!this.sugarCurrentUser.teams.includes(team.id)) {
-            this.sugarCurrentUser.teams.push(team.id);
-        } else {
-            const index = this.sugarCurrentUser.teams.indexOf(team.id);
-            this.sugarCurrentUser.teams.splice(index, 1);
-        }
-    }
-
     public trackByFn(item) {
         return item.id;
     }
-
 }
