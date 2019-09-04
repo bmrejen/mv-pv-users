f81abd5746ddd017d80a6f84d619ac2dfba1d62e
Benoit <bmrejen@marcovasco.fr>
2019-09-04 13:04:00 +0800

    feat(users): 18342 top navbar

--- tests/unit/app.component.spec.ts
+++ tests/unit/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { AppComponent } from "../../src/app.component";
+import { NavbarComponent } from "./../../src/app/components/navbar/navbar.component";
 
 describe("AppComponent", () => {
     beforeEach(async(() => {
@@ -8,12 +9,13 @@ describe("AppComponent", () => {
         TestBed.configureTestingModule({
             declarations: [
                 AppComponent,
+                NavbarComponent,
             ],
             imports: [
                 RouterTestingModule,
             ],
         })
-        .compileComponents();
+            .compileComponents();
     }));
 
     it("should create the component", async(() => {
